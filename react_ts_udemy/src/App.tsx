3f5f09d7f211de68ffd1c07058230c85708a099b
youjin kim <gracecancho@gmail.com>
2023-10-26 16:33:33 +0900

    newTodo

--- react_ts_udemy/src/App.tsx
+++ react_ts_udemy/src/App.tsx
@@ -1,16 +1,24 @@
+import { useState } from 'react';
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  // NewTodo를 통해 todo가 추가되면 todos가 동적으로 변해야함
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript'),
-  ];
+  // NewTodo를 통해 todo가 추가되면 todos가 동적으로 변해야함 
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // const todos = [
+  //   new Todo('Learn React'),
+  //   new Todo('Learn TypeScript'),
+  // ];
+
 
   const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
 
+    setTodos((preveTodos) => {
+      return preveTodos.concat(newTodo);
+    })
   }
 
   return (
