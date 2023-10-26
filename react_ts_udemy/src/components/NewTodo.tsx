cab78bd7b4a7ce6ac238d13599de28430ec0edc9
youjin kim <gracecancho@gmail.com>
2023-10-26 16:12:53 +0900

    input에 useRef 추가

--- react_ts_udemy/src/components/NewTodo.tsx
+++ react_ts_udemy/src/components/NewTodo.tsx
@@ -1,12 +1,23 @@
+import { useRef } from 'react';
+
 const NewTodo = () => {
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
+
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
+
+        // null이 아니라는 확신이 있을 때 '!' 느낌표를 사용
+        const enteredText = todoTextInputRef.current!.value;
+        if (enteredText.trim().length === 0) {
+            // throw an error
+            return;
+        }
     }
 
     return (
         <form onSubmit={submitHandler}>
             <label htmlFor="text">Todo text</label>
-            <input type="text" id="text" />
+            <input type="text" id="text" ref={todoTextInputRef} />
             <button>Add Todo</button>
         </form>
     )
