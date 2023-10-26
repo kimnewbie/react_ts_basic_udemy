4ce326c4523d81fdc1b4937d24dbc0d6c89e3d1b
youjin kim <gracecancho@gmail.com>
2023-10-26 16:18:49 +0900

    Function Props로 작업하기

--- react_ts_udemy/src/App.tsx
+++ react_ts_udemy/src/App.tsx
@@ -1,14 +1,21 @@
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
+  // NewTodo를 통해 todo가 추가되면 todos가 동적으로 변해야함
   const todos = [
     new Todo('Learn React'),
     new Todo('Learn TypeScript'),
   ];
 
+  const addTodoHandler = (todoText: string) => {
+
+  }
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
